73e43b76d38c8b4ac94774bf5f8e5c80bb20472e
lucasdure <lucas.dure@edu.esiee.fr>
2020-12-24 23:23:10 +0100

    Initial commit

---
+++ Source/CreatureCreator/CreatureCreator.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "CreatureCreator.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, CreatureCreator, "CreatureCreator" );
---
+++ Source/CreatureCreator/CreatureCreatorGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "CreatureCreatorGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CREATURECREATOR_API ACreatureCreatorGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
